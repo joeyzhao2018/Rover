3da12202b5c198bd2bca8b850e25b4d3372ba264
Joey Zhao <joeyzhao2018@gmail.com>
2017-06-01 09:05:06 -0400

    using the same method in DEMO: run_direct

--- world/abstract/orientation/tests/test_directions.py
+++ world/abstract/orientation/tests/test_directions.py
@@ -1,10 +1,13 @@
 from world.abstract.orientation.directions import Direction, opposite
 
+print(_json_config)
 
 a=Direction.NORTH
 b=Direction.SOUTH
 c=Direction.EAST
 d=Direction.WEST
+
+print(str(a))
 
 assert opposite(a)==b
 assert opposite(b)==a
--- world/robots.py
+++ world/robots.py
@@ -3,6 +3,11 @@ from world.abstract.orientation.directions import Direction, opposite
 from world.abstract.maps.Tile import Tile
 from world.ev3controls import movements
 
+_turn_map={
+
+}
+
+
 class MyCompanion(object):
 
     facingDirection=Direction.NORTH
@@ -16,3 +21,15 @@ class MyCompanion(object):
             pass
         elif self.facingDirection==opposite(direction):
             movements.turnback()
+        else:
+            left_yes=(self.facingDirection-direction)%2 if self.facingDirection<2 else (self.facingDirection-direction)%2-1
+            #Because if N or S, you need to turn left when the difference is odd; if W or E, turn left when the difference is even
+            if left_yes:
+                movements.turnleft()
+            else:
+                movements.turnright()
+
+
+
+    def go_straight_timed(self,time):
+        movements.run_direct()
